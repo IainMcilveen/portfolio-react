7190375423e147c21502b215940147b7223bf77f
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-11-24 22:52:51 -0500

    added sass

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,11 @@ import profile from './img/profile.jpg';
 function Main(){
     return(
         <div className="App-main">
-            <p>main</p>
-            <div className="name">
-                <p>Iain Mcilveen</p>
-            </div>
             <div className="profile">
                 <img src={profile} alt="nope" />
+            </div>
+            <div className="name">
+                <p>Iain Mcilveen</p>
             </div>
         </div>
     );
