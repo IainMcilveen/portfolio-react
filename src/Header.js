0eae1c761afc4ceba654ce26813ff8a6defca5c6
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-01-17 00:59:10 -0500

    links and styles

--- src/Header.js
+++ src/Header.js
@@ -12,6 +12,12 @@ function Header(){
                 <h1>Iain Mcilveen</h1>
                 <h2>Aspiring Web Developer and Software Engineer</h2>
             </div>
+            <div className="My-links">
+                <a href="https://github.com/IainMcilveen"><i class="fa fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/iain-mcilveen-64b14a185/"><i class="fa fa-linkedin"></i></a>
+                <a href="./other/resume.pdf"><i class="fa fa-file"></i></a>
+            </div>
+            
             
             
         </div>
