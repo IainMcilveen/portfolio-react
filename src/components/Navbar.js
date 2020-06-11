823ac4baf144ee5519045e56fd27a656b394fa78
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-06-11 20:34:35 -0300

    added navbar links and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,25 @@ import React from "react"
 import "../css/Navbar.scss"
 
 function Navbar(){
+    
     return(
-        <div>
-            Hello
-        </div>
+        <nav class="navbar">
+            <ul class="navbar-list">
+                <li class="navbar-element">
+                    <a class="navbar-link" href="#About">About Me</a>
+                </li>
+                <li class="navbar-element">
+                    <a class="navbar-link" href="#Experiences">Experiences</a>
+                </li>
+                <li class="navbar-element">
+                    <a class="navbar-link" href="#Projects">Projects</a>
+                </li>
+                <li class="navbar-element">
+                    <a class="navbar-link" href="#Contact">Contact</a>
+                </li>
+            </ul>
+
+        </nav>
     );
 }
 
