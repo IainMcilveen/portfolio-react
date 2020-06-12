3506eb5b3561f9c7b02aa6249aac245ed61df3ed
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-06-11 21:44:31 -0300

    nav bar hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,48 @@
-import React from "react"
+import React , { useState } from "react"
 import "../css/Navbar.scss"
 
 function Navbar(){
+
+    const [hoverIndex,setHoverIndex] = useState(-1);
+    const [navHidden,setNavHidden] = useState(false);
+
+    let options = [
+        {
+            text: "About Me",
+            loc: "#About"
+        },
+        {
+            text: "Experiences",
+            loc: "#Experiences"
+        },
+        {
+            text: "Projects",
+            loc: "#Projects"
+        },
+        {
+            text:"Contact",
+            loc:"#Contact"
+        }]; 
+    let links = [];
+
+
+    for(let i = 0; i < options.length; i++){
+        links.push(
+            <li class="navbar-element"
+                onMouseEnter={ () => setHoverIndex(i)}
+                onMouseLeave={ () => setHoverIndex(-1)}
+                style={{"background-color": (hoverIndex === i ? '#323742' : "") }}
+            >
+                <a class="navbar-link" href={options[i].loc}>{options[i].text}</a>
+            </li>
+        )
+    }
+
     
     return(
         <nav class="navbar">
             <ul class="navbar-list">
-                <li class="navbar-element">
-                    <a class="navbar-link" href="#About">About Me</a>
-                </li>
-                <li class="navbar-element">
-                    <a class="navbar-link" href="#Experiences">Experiences</a>
-                </li>
-                <li class="navbar-element">
-                    <a class="navbar-link" href="#Projects">Projects</a>
-                </li>
-                <li class="navbar-element">
-                    <a class="navbar-link" href="#Contact">Contact</a>
-                </li>
+                {links}
             </ul>
 
         </nav>
