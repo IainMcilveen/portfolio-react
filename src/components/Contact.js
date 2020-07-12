37b5c19a8ad2b29a50cafa518c53618f0a437611
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-07-12 16:05:43 -0300

    scroll to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,9 @@ import '../css/Contact.scss';
 function Contact(){
     return(
         <div className="App-Contact">
+            <scroll-container>
+                <scroll-page id="Contact"></scroll-page>
+            </scroll-container>
             BruhMoment
             <a href="https://github.com/IainMcilveen">Github</a>
             <a href="https://www.linkedin.com/in/iain-mcilveen-64b14a185/">Linkedin</a>
