1f3e29b1d2057d02ff2a9b368a193c4b6ccfce8c
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-08-24 14:06:10 -0300

    made contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,22 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faScroll,faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import '../css/Contact.scss';
 
 function Contact(){
+    const resumeIcon = <FontAwesomeIcon icon={faScroll} />
+    const gitIcon = <FontAwesomeIcon icon={faGithub} />
+    const linkedIcon = <FontAwesomeIcon icon={faLinkedin} />
     return(
         <div className="App-Contact">
             <scroll-container>
                 <scroll-page id="Contact"></scroll-page>
             </scroll-container>
-            BruhMoment
-            <a href="https://github.com/IainMcilveen">Github</a>
-            <a href="https://www.linkedin.com/in/iain-mcilveen-64b14a185/">Linkedin</a>
-            <a href="./other/resume.pdf">Resume</a>
+            <h1>Find Me</h1>
+            <a href="https://github.com/IainMcilveen">{gitIcon}</a>
+            <a href="https://www.linkedin.com/in/iain-mcilveen-64b14a185/">{linkedIcon}</a>
+            <a href="./other/resume.pdf">{resumeIcon}</a>
         </div>
     );
 }
